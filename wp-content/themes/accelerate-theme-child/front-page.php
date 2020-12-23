dc077d5b6159a187b97bf8cdacdd31197d51c78c
ryoko-y <wagarden6@gmail.com>
2020-12-23 14:41:29 -0500

    Add case studies and icons

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -15,16 +15,39 @@
  */
 
 get_header(); ?>
-	<div id="primary" class="home-page hero-content">
+
+<section id="primary" class="home-page hero-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+				<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
-	</div><!-- #primary -->
+</section><!-- #primary -->
 
+<section class="featured-work">
+	  <div class="site-content">
+			 <h4>Featured Work</h4>
+
+			 <ul class="homepage-featured-work">
+			 <?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+				 <?php while ( have_posts() ) : the_post();
+					 $image_1 = get_field("image_1");
+					 $size = "medium";
+					?>
+					<li class="individual-featured-work">
+					 <figure><a href="<?php the_permalink(); ?>">
+						 <?php if($image_1){
+							 echo wp_get_attachment_image($image_1,$size); }?></a>
+					 </figure>
+					 <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+				  </li>
+				 <?php endwhile; //end of the loop ?>
+			 <?php wp_reset_query(); //resets the altered query back to the original ?>
+       </ul>
+	  </div>
 <section class="recent-posts">
+
   <div class="site-content">
 		<div class="blog-post">
       <h4>From the Blog</h4>
@@ -32,8 +55,8 @@ get_header(); ?>
           <?php while ( have_posts() ) : the_post(); ?>
 	          <h3><?php the_title(); ?></h3>
 	          <?php the_excerpt(); ?>
-          <?php endwhile; ?>
-        <?php wp_reset_query(); ?>
+          <?php endwhile; //end of the loop ?>
+        <?php wp_reset_query(); //resets the altered query back to the original ?>
     </div>
   </div>
 </section>
--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -16,3 +16,22 @@ function accelerate_child_scripts(){
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+// Custom post types functions
+function create_custom_post_types() {
+// Create a case study custom post type
+    register_post_type( 'case_studies',
+        array(
+            'labels' => array(
+                'name' => __( 'Case Studies' ),
+                'singular_name' => __( 'Case Study' )
+                ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array(
+							'slug' => 'case-studies'
+						    ),
+            )
+     );
+}
+add_action( 'init', 'create_custom_post_types' );
